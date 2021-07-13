99a17c57c231cd7d33f5e0e837c2df25824fa5c3
VishalGhai <vghai2710@gmail.com>
2021-07-13 19:30:57 +0530

    LogIn and SignUp screen UI done.

--- App.js
+++ App.js
@@ -4,20 +4,24 @@ import HomeScreen from './Screens/HomeScreen'
 import TransactionScreen from './Screens/Transactions'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import LogInScreen from './Screens/LogInScreen'
+import SignUpScreen from './Screens/SignUpScreen'
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StatusBar backgroundColor={'white'} />
+      <StatusBar backgroundColor="rgba(0,0,0,0)" transparent={true} translucent={true} />
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName="SignUpScreen"
         screenOptions={{
           headerShown: false
         }}
 
       >
+        <Stack.Screen name="LogInScreen" component={LogInScreen} />
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="TransactionScreen" component={TransactionScreen} />
       </Stack.Navigator>
---
+++ Screens/LogInScreen.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { Image, ImageBackground, Keyboard, KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
+
+const LogInScreen = () => {
+
+
+    return (
+        <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
+            <ImageBackground style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }} source={require('../Assets/Images/LogInScreenBG.png')}>
+                <Image source={require('../Assets/Icons/MainAppIcon.png')} style={{ width: 100, height: 100 }} />
+                <View style={styles.InputContainer}>
+                    <TextInput style={styles.Input} placeholder="Enter Username" placeholderTextColor="lightgrey" ></TextInput>
+                    <TextInput style={styles.Input} placeholder="Enter Password" placeholderTextColor="lightgrey" ></TextInput>
+                </View>
+                <TouchableOpacity style={{ width: 180, height: 60 }}>
+                    <ImageBackground style={{ width: '100%', height: '100%' }} source={require('../Assets/Images/LogInBtnActive.png')} />
+                </TouchableOpacity>
+                <View style={{ display: 'flex', flexDirection: 'row', marginTop: 50 }}>
+                    <TouchableOpacity style={{ width: 150, height: 60 }}>
+                        <ImageBackground style={{ width: '100%', height: '100%' }} source={require('../Assets/Images/SignUpBtn.png')} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ width: 150, height: 60 }}>
+                        <ImageBackground style={{ width: '100%', height: '100%' }} source={require('../Assets/Images/GoogleSignInBtn.png')} />
+                    </TouchableOpacity>
+
+                </View>
+            </ImageBackground>
+        </TouchableWithoutFeedback>
+    )
+}
+
+export default LogInScreen
+
+const styles = StyleSheet.create({
+    InputContainer: {
+        width: '80%',
+        marginTop: 40,
+        marginBottom: 20
+    },
+    Input: {
+        width: '100%',
+        height: 45,
+        borderColor: 'grey',
+        borderRadius: 15,
+        paddingHorizontal: 20,
+        marginVertical: 15,
+        fontSize: 22,
+        color: 'black',
+        elevation: 3,
+        backgroundColor: 'white'
+    }
+})
